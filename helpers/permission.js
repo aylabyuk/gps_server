a17f96543fbda05449da0f2173e593b961506fa6
oriel <oriel.absin@gmail.com>
2017-12-07 15:50:30 +0800

    whitespaces

--- helpers/permission.js
+++ helpers/permission.js
@@ -2,21 +2,21 @@ import { createResolver } from '../graphql-sequelize-crud-aylabyuk/src'
 
 export const requiresAuth = createResolver((parent, args, context) => {
     if (!context.user || !context.user.id) {
-      throw new Error('Not authenticated');
+        throw new Error('Not authenticated');
     }
 });
   
-  export const requiresStaff = requiresAuth.createResolver((parent, args, context) => {
-      if(!context.user.isStaff) {
+export const requiresStaff = requiresAuth.createResolver((parent, args, context) => {
+    if(!context.user.isStaff) {
         throw new Error('Action not permitted for non-GPS staff')
-      }
+    }
 })
   
-  export const requiresAdmin = requiresAuth.createResolver(
+export const requiresAdmin = requiresAuth.createResolver(
     (parent, args, context) => {
-      if (!context.user.isAdmin) {
-        throw new Error('Requires admin access');
-      }
+        if (!context.user.isAdmin) {
+            throw new Error('Requires admin access');
+        }
     },
 );
   
